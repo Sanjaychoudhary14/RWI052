d28ec3d0459453918adc54b401984e359a7f09d8
Sanjaychoudhary14 <162950551+Sanjaychoudhary14@users.noreply.github.com>
2024-04-03 12:22:00 +0530

    Add files via upload

---
+++ Overloading.java
@@ -0,0 +1,34 @@
+public class Overloading
+ { 
+    // Overloaded sum(). This sum takes two int parameters 
+    
+	public int sum(int x, int y)
+	{
+		return (x + y);
+	} 
+  
+    // Overloaded sum(). This sum takes three int parameters 
+    
+	public int sum(int x, int y, int z) 
+    { 
+        return (x + y + z); 
+    } 
+  
+    // Overloaded sum(). This sum takes two double 
+    // parameters 
+    
+	public double sum(double x, double y) 
+    { 
+        return (x + y); 
+    } 
+  
+    // Driver code 
+    
+	public static void main(String args[]) 
+    { 
+        Overloading ov = new Overloading(); 
+        System.out.println(ov.sum(10, 20)); 
+        System.out.println(ov.sum(10, 20, 30)); 
+        System.out.println(ov.sum(10.5, 20.5)); 
+    } 
+}
